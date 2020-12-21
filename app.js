5eb27c1ce16c93a4e7588746c401a0c0bd207dd6
IhorS <caxnoigor@gmail.com>
2020-12-21 12:07:59 +0200

    add

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
 
-// if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, './client/build')))
 
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./client/build/index.html'))
   })
-// }
+}
 
 app.use((err, req, res, next) => {
   res
